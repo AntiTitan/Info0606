d85f2dfb44c17d3d1110465d3fe6cd2db6a0a8d9
AntiTitan <antineapintaux@gmail.com>
2020-02-15 17:16:47 +0100

    modif

---
+++ Robot/Assets/Scripts/StartGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    GameObject StartQuad;
+    Renderer Robot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartQuad = GameObject.FindGameObjectWithTag("StartQuad");
+        Robot = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+}
